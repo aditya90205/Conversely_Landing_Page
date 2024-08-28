df064ecfb9fdc0751148fa97c7af8ec7b00f1c2a
aditya90205 <adityasrivastav90205@gmail.com>
2024-08-28 18:23:00 +0530

    added about section layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,15 @@
 const About = () => {
   return (
-    <div className="bg-blue-200 p-6 rounded-2xl text-center font-sans text-black mt-4 common mb-3">
-      <div className="p-10">
-      <h2 className="text-2xl font-bold mb-4">About</h2>
-      <p>
-        Conversely helps businesses grow by offering targeted, full-funnel
-        marketing solutions that drive engagement and conversions. With advanced
-        strategies like social media campaigns, lead generation, and WhatsApp
-        bot integration, Conversely optimizes the entire customer journey. Their
-        approach combines creativity, consumer insights, and technology to
-        deliver results that align with business goals and enhance customer
+    <div className="bg-blue-200 p-6 rounded-[40px] text-center font-sans text-black mt-4 common mb-3 w-[1484.43px] h-[381.37px]">
+      <div className="w-[974.04px] h-[266px] m-auto mt-6 flex flex-col items-center justify-center">
+      <h2 className="text-3xl font-bold mb-4 p-[10px] gap-[10px] w-[745.5px] h-[67px]">About</h2>
+      <p className="text-xl">
+        Conversely helps businesses grow by offering targeted, full-funnel <br/>
+        marketing solutions that drive engagement and conversions. With<br/> advanced
+        strategies like social media campaigns, lead generation, and<br/> WhatsApp
+        bot integration, Conversely optimizes the entire customer <br/> journey. Their
+        approach combines creativity, consumer insights, and <br/>technology to
+        deliver results that align with business goals and enhance <br/> customer
         relationships.
       </p>
       </div>
