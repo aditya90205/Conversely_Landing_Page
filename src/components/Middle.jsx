0813a260a95c2749ec25ea43e62576b321786c1e
aditya90205 <adityasrivastav90205@gmail.com>
2024-08-28 10:08:34 +0530

    feat: Update font size and spacing in Middle component
    Adjusted the font size and added margin-bottom to the percentage values in the Middle component for better readability and visual consistency.

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -9,19 +9,19 @@ const Middle = () => {
         </h2>
         <div className="flex justify-between space-x-12 mt-9">
           <div className="text-center">
-            <h3 className="text-4xl font-bold text-black">44%</h3>
+            <h3 className="text-2xl font-bold text-black mb-2">44%</h3>
             <p className="text-lg text-gray-700">
               User’s using Instagram for <br /> shopping (Weekly)
             </p>
           </div>
           <div className="text-center">
-            <h3 className="text-4xl font-bold text-black">1400% / 20%</h3>
+            <h3 className="text-2xl font-bold text-black mb-2">1400% / 20%</h3>
             <p className="text-lg text-gray-700">
-              Increase in traffic and <br /> boosted revenue
+              Increase in traffic and  boosted <br /> revenue
             </p>
           </div>
           <div className="text-center">
-            <h3 className="text-4xl font-bold text-black">500 million</h3>
+            <h3 className="text-2xl font-bold text-black mb-2">500 million</h3>
             <p className="text-lg text-gray-700">
               User’s using Instagram & <br /> Whatsapp for business purpose
             </p>
@@ -31,8 +31,8 @@ const Middle = () => {
     </div>
 
       {/* Benefits Section */}
-      <section className="text-center py-10">
-        <p className="text-lg text-gray-600 mb-2">What you will get</p>
+      <section className="text-center pb-5">
+        <p className="text-xl text-gray-600 mb-2">What you will get</p>
         <h2 className="text-2xl font-semibold text-black">
           Features to use Whatsapp bot
         </h2>
