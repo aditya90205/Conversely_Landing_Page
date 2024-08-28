d990ef817ac86f60cb213a8a0dde8e0331344af9
aditya90205 <adityasrivastav90205@gmail.com>
2024-08-28 11:04:41 +0530

    Update QRCodeSection image size and position for better visual layout and alignment

--- src/components/QRCodeSection.jsx
+++ src/components/QRCodeSection.jsx
@@ -6,14 +6,14 @@ const QRCodeSection = () => {
     <div className="flex flex-row bg-blue-50 rounded-md common">
       <div className="flex justify-between p-10">
         <div className="ml-10 grid relative">
-          <img src={img} alt="QR1 Logo" className=" w-[508.63px] rounded-md" />
+          <img src={img} alt="QR1 Logo" className=" w-[31rem] h-[31rem] rounded-md" />
           <img
             src={qrImage}
             alt="QRCode"
-            className="h-20 absolute top-[15.1rem] right-[10.1rem] rounded-lg"
+            className="h-20 absolute top-[14.7rem] right-[9.8rem] rounded-lg"
           />
         </div>
-        <div className="mt-36 p-6 w-[589px]">
+        <div className="mt-32 p-6 w-[589px]">
           <div className="text-left">
             <div className="mb-4 flex items-center
             ">
@@ -33,7 +33,7 @@ const QRCodeSection = () => {
             </h2>
 
             {/* Description */}
-            <p className="text-gray-600 text-lg leading-relaxed">
+            <p className="text-gray-600 text-lg leading-relaxed w-[23.6rem]">
               Shop easily from your phone! Just scan the QR code outside our
               store to connect with our WhatsApp bot. Browse products, get
               recommendations, and make purchases—all through WhatsApp.
